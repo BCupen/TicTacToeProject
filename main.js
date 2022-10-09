f132eba0cc252ccf0e54a36fd99f61ab7bed6f8f
BCupen <bacupen@gmail.com>
2022-10-09 11:38:53 -0400

    title screen completed

---
+++ main.js
@@ -0,0 +1,16 @@
+//get buttons
+const playPvPButton =  document.querySelector('.PvP');
+const playAIButton = document.querySelector('.AI');
+
+//get containers
+const menuC = document.querySelector('.menu-container');
+const gameC = document.querySelector('.game-container');
+
+playPvPButton.addEventListener('click', function(e){
+    menuC.style.display = 'none';
+    gameC.style.display = 'block';
+});
+
+const GameBoard = (() =>{
+
+})();
