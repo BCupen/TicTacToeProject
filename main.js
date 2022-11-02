e8e1fe0ac3ef877444fbdf98e4696f1a5ab9db3d
BCupen <bacupen@gmail.com>
2022-11-02 18:56:06 -0400

    added check win and tie, score increment

--- main.js
+++ main.js
@@ -33,10 +33,10 @@ const GameBoard = (() =>{
     ];
 
     let board = Array(9).fill('');
+    let turns = 0;
 
     const startGame = () =>{
         const coinFlip = Math.floor(Math.random() * 2);
-        console.log(coinFlip);
         if(coinFlip === 1){
             player2.char = 'X';
             player1.char = 'O';
@@ -47,23 +47,68 @@ const GameBoard = (() =>{
         }, 2000);
     };
 
+    const checkWin = (mark) =>{
+        let win;
+        for(let combo of winCombos){
+            win = true;
+            for(let pos of combo){
+                if(board[pos] !== mark){
+                    win = false;
+                }
+            }
+            if(win){
+                return {win, combo};
+            }
+        }
+        return {win, combo: []};
+    }
+
+
     //get board
     const spaces  = document.querySelectorAll('.space');
+
+    //get player scores
+    const player1Score = document.querySelector('.first').nextElementSibling;
+    const player2Score = document.querySelector('.second').nextElementSibling;
 
     spaces.forEach(space =>{
         space.addEventListener('click', (e)=>{
             const el = e.target;
             const index = el.getAttribute('data-index');
             if(!el.textContent){
+                turns++;
                 board[index] = currPlayer.char;
                 el.textContent = currPlayer.char;
                 el.style.color = currPlayer.color;
                 currPlayer.moves.push(index);
-                if(currPlayer === player1)
-                    currPlayer = player2;
-                else currPlayer = player1;
+                const checkWinObj = checkWin(currPlayer.char);
+                if (checkWinObj.win){
+                    header.textContent = `${currPlayer.name} wins!`;
+                    currPlayer.score++;
+                    if(currPlayer.name === 'Player 1'){
+                        player1Score.textContent = currPlayer.score;
+                    }else{
+                        player2Score.textContent = currPlayer.score;
+                    }
+                    for(let pos of checkWinObj.combo){
+                        const currSpace = document.querySelector(`[data-index='${pos}']`);
+                        currSpace.style.color = '#22c55e';
+                    }
+                }else{
+                    if(turns >= 9){
+                        player1Score.textContent = parseInt(player1Score.textContent) + 1;
+                        player2Score.textContent = parseInt(player2Score.textContent) + 1;
+                        header.textContent = `It's a Tie!`;
+                    }else{
+                        if(currPlayer === player1)
+                            currPlayer = player2;
+                        else currPlayer = player1;
 
-                header.textContent = `${currPlayer.name}! Your turn.`;
+                        header.textContent = `${currPlayer.name}! Your turn.`;
+                    }
+                    
+                }
+                
             }          
             else{
                 header.textContent = `You cannot play here`;
